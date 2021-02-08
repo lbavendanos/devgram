6444048926333a3b19e62c4913e1d2f2ec65ea38
lbavendanos <lbavendanos@gmail.com>
2021-02-08 01:10:58 -0500

    Fix home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,50 @@
-import Head from 'next/head'
-import Footer from '@/components/Footer/Footer'
-import Navbar from '@/components/Navbar/Navbar'
+import firebase from 'firebase/app'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/dist/client/router'
+import { app } from '@/utils/firebase/firebase'
+import { AuthContext } from '@/contexts/AuthContext'
+import { User } from '@/types'
+import AuthLayout from '@/components/Layouts/AuthLayout/AuthLayout'
+import BasicLayout from '@/components/Layouts/BasicLayout/BasicLayout'
+import SocialSigIn from '@/components/Partials/SocialSignIn/SocialSignIn'
 
 export default function Home(): JSX.Element {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Head>
-        <title>Devgram</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [user, setUser] = useState<User | null>(null)
+  const router = useRouter()
 
-      <Navbar />
-      <main className="flex-grow bg-gray-100"></main>
-      <Footer className="mt-auto" />
-    </div>
+  const handleGithubSignInSuccess = () => {
+    router.push('/')
+  }
+
+  const hanbleGithubSingInError = (error: Error) => {
+    console.log(error)
+  }
+
+  const transformUserAuth = (userAuth: firebase.User) => {
+    const name = userAuth.displayName || ''
+    const email = userAuth.email || ''
+    const avatar = userAuth.photoURL || ''
+
+    return { name, email, avatar }
+  }
+
+  useEffect(() => {
+    app.auth().onAuthStateChanged((userAuth) => {
+      setUser(userAuth ? transformUserAuth(userAuth) : null)
+    })
+  }, [])
+
+  return user ? (
+    <AuthContext.Provider value={user}>
+      <AuthLayout></AuthLayout>
+    </AuthContext.Provider>
+  ) : (
+    <BasicLayout className="flex justify-center items-center">
+      <SocialSigIn
+        githubSigIn
+        onGithubSignInSuccess={handleGithubSignInSuccess}
+        onGithubSignInError={hanbleGithubSingInError}
+      />
+    </BasicLayout>
   )
 }
