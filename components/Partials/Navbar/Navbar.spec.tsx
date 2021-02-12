a8b633dfa93ee3f1d44b17e9f9a13b8b46e57b22
lbavendanos <lbavendanos@gmail.com>
2021-02-12 01:24:22 -0500

    Fix navbar

--- components/Partials/Navbar/Navbar.spec.tsx
+++ components/Partials/Navbar/Navbar.spec.tsx
@@ -1,5 +1,27 @@
 import { render, screen } from '@testing-library/react'
+import firebase from 'firebase'
+import { Auth, useAuth } from '@/contexts/AuthContext'
 import Navbar from './Navbar'
+
+jest.mock('@/contexts/AuthContext')
+jest.mock('@/components/Partials/UserDropdown/UserDropdown', () => {
+  return function DummyUserDropdown() {
+    return <div>DummyUserDropdown</div>
+  }
+})
+
+const mockUseAuth = useAuth as jest.MockedFunction<typeof useAuth>
+
+beforeEach(() => {
+  jest.clearAllMocks()
+
+  const auth: Auth = {
+    user: { displayName: 'foo' } as firebase.User,
+    loading: true,
+  }
+
+  mockUseAuth.mockReturnValue(auth)
+})
 
 describe('Navbar component', () => {
   it('render logo', () => {
@@ -22,5 +44,20 @@ describe('Navbar component', () => {
     expect(explorer).toBeInTheDocument()
     expect(activity).toBeInTheDocument()
     expect(user).toBeInTheDocument()
+  })
+
+  it('render social login', () => {
+    const auth: Auth = {
+      user: null,
+      loading: true,
+    }
+
+    mockUseAuth.mockReturnValue(auth)
+
+    render(<Navbar />)
+
+    const githubButton = screen.getByText('Login with GitHub')
+
+    expect(githubButton).toBeInTheDocument()
   })
 })
--- components/Partials/Navbar/Navbar.tsx
+++ components/Partials/Navbar/Navbar.tsx
@@ -1,38 +1,68 @@
 import Link from 'next/link'
 import { FaHome, FaHeart, FaWpexplorer } from 'react-icons/fa'
 import UserDropdown from '@/components/Partials/UserDropdown/UserDropdown'
+import { useAuth } from '@/contexts/AuthContext'
+import GithubButton from '../GithubButton/GithubButton'
+import { useRouter } from 'next/router'
 
-export default function Navbar(): JSX.Element {
+export default function Navbar() {
+  const { user } = useAuth()
+  const router = useRouter()
+
+  const handleGithubSignInSuccess = () => {
+    router.push('/')
+  }
+
+  const handleGithubSignInError = (error: Error) => {
+    console.log(error)
+  }
+
   return (
     <header className="sticky z-40 top-0 border-b border-gray-300">
       <div className="container mx-auto flex p-4">
         <div className="flex-1">
-          <h1 className="font-mono text-xl text-black">Devgram</h1>
+          <Link href="/">
+            <a className="outline-none" role="button" tabIndex={0}>
+              <h1 className="font-mono text-xl text-black">Devgram</h1>
+            </a>
+          </Link>
         </div>
-        <nav className="flex-1">
-          <ul className="flex justify-end">
-            <li className="" data-testid="home">
-              <Link href="/">
-                <a className="block" role="button" tabIndex={0}>
-                  <FaHome className="w-6 h-6" />
+        {user ? (
+          <nav className="flex-1">
+            <ul className="flex justify-end">
+              <li className="" data-testid="home">
+                <Link href="/">
+                  <a className="block outline-none" role="button" tabIndex={0}>
+                    <FaHome className="w-6 h-6" />
+                  </a>
+                </Link>
+              </li>
+              <li className="ml-6" data-testid="explorer">
+                <a className="block outline-none">
+                  <FaWpexplorer className="w-6 h-6" />
                 </a>
-              </Link>
-            </li>
-            <li className="ml-6" data-testid="explorer">
-              <a className="block">
-                <FaWpexplorer className="w-6 h-6" />
-              </a>
-            </li>
-            <li className="ml-6" data-testid="activity">
-              <a className="block">
-                <FaHeart className="w-6 h-6" />
-              </a>
-            </li>
-            <li className="ml-6" data-testid="user">
-              <UserDropdown />
-            </li>
-          </ul>
-        </nav>
+              </li>
+              <li className="ml-6" data-testid="activity">
+                <a className="block outline-none">
+                  <FaHeart className="w-6 h-6" />
+                </a>
+              </li>
+              <li className="ml-6" data-testid="user">
+                <UserDropdown />
+              </li>
+            </ul>
+          </nav>
+        ) : (
+          <div className="flex-1">
+            <div className="flex justify-end">
+              <GithubButton
+                className="text-xs text-center px-3"
+                onGithubSignInSuccess={handleGithubSignInSuccess}
+                onGithubSignInError={handleGithubSignInError}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </header>
   )
