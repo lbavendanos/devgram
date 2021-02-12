6563b1266ac5dc703727435dd1f39172b7509e68
lbavendanos <lbavendanos@gmail.com>
2021-02-12 01:25:17 -0500

    Fix home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,34 @@
 import { useAuth } from '@/contexts/AuthContext'
 import AuthLayout from '@/components/Layouts/AuthLayout/AuthLayout'
 import BasicLayout from '@/components/Layouts/BasicLayout/BasicLayout'
-import SocialAuth from '@/components/Partials/SocialAuth/SocialAuth'
+import React from 'react'
+import GithubButton from '@/components/Partials/GithubButton/GithubButton'
 
 export default function Home(): JSX.Element {
   const { user, loading } = useAuth()
+  const title = 'Devgram'
 
-  const hanbleGithubSingInError = (error: Error) => {
+  const handleGithubSignInError = (error: Error) => {
     console.log(error)
   }
 
   if (!loading) return <div className="font-mono">Loading...</div>
 
   return user ? (
-    <AuthLayout></AuthLayout>
+    <AuthLayout title={title}></AuthLayout>
   ) : (
-    <BasicLayout className="flex justify-center items-center">
-      <SocialAuth githubSigIn onGithubSignInError={hanbleGithubSingInError} />
+    <BasicLayout title={title} className="flex justify-center items-center">
+      <div className="container mx-auto">
+        <div className="text-center mb-2">
+          <h1 className="font-mono text-3xl text-black">Devgram</h1>
+          <p className="font-mono text-md text-gray-600">
+            the social network for developers
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <GithubButton onGithubSignInError={handleGithubSignInError} />
+        </div>
+      </div>
     </BasicLayout>
   )
 }
