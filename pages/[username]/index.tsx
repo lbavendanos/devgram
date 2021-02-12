f3f64a6e1421984cbc0b01c05d0b4e06a7e3c13b
lbavendanos <lbavendanos@gmail.com>
2021-02-12 01:25:29 -0500

    Fix profile page

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,8 +1,17 @@
 import { useRouter } from 'next/router'
+import { useAuth } from '@/contexts/AuthContext'
+import AuthLayout from '@/components/Layouts/AuthLayout/AuthLayout'
 
 export default function Profile(): JSX.Element {
+  const { loading } = useAuth()
   const router = useRouter()
   const { username } = router.query
 
-  return <div>{username}</div>
+  if (!loading) return <div className="font-mono">Loading...</div>
+
+  return (
+    <AuthLayout title="Devgram">
+      <div>{username}</div>
+    </AuthLayout>
+  )
 }
