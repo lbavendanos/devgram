afd9a2e3b26ff099d1026d43cdd11e0cf58614a6
lbavendanos <lbavendanos@gmail.com>
2021-02-12 01:23:13 -0500

    Add profile hook

---
+++ hooks/profile.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import firebase from 'firebase'
+import { Profile } from '@/types'
+import { db } from '@/utils/firebase/firebase'
+
+export function useProfile(user: firebase.User | null) {
+  const [profile, setProfile] = useState<Profile | null>(null)
+
+  const fetchProfile = async (id: string) => {
+    let profile = null
+
+    if (id) {
+      try {
+        const documentSnapshot = await db.collection('profiles').doc(id).get()
+
+        profile = documentSnapshot.exists
+          ? (documentSnapshot.data() as Profile)
+          : null
+      } catch (error) {
+        console.log('Error getting document:', error)
+      }
+    }
+
+    return profile
+  }
+
+  useEffect(() => {
+    if (user) {
+      fetchProfile(user.uid).then((profile) => {
+        setProfile(profile)
+      })
+    }
+  }, [user])
+
+  return { profile }
+}
