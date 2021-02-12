3c892ed79f528eec00156ba1040328a78ba0ac1b
lbavendanos <lbavendanos@gmail.com>
2021-02-12 16:45:54 -0500

    Add server side props

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,17 +1,68 @@
-import { useRouter } from 'next/router'
+import { GetServerSidePropsContext } from 'next'
+import { ParsedUrlQuery } from 'querystring'
+import { db } from '@/utils/firebase/admin'
+import { Profile } from '@/types'
 import { useAuth } from '@/contexts/AuthContext'
 import CustomLayout from '@/components/Layouts/CustomLayout/CustomLayout'
 
-export default function Profile(): JSX.Element {
+interface Props {
+  profile: Profile
+}
+
+interface Params extends ParsedUrlQuery {
+  username: string
+}
+
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext<Params>
+) => {
+  let profile: Profile | null = null
+  const { params } = context
+
+  if (params) {
+    const { username } = params
+
+    if (username) {
+      try {
+        const documentSnapshot = await db
+          .collection('profiles')
+          .where('username', '==', username)
+          .limit(1)
+          .get()
+
+        if (documentSnapshot.empty) {
+          profile = null
+          console.log(`No matching documents with ${username} username.`)
+        } else {
+          documentSnapshot.forEach((documentData) => {
+            profile = documentData.data() as Profile
+          })
+        }
+      } catch (error) {
+        console.log('Error getting document:', error)
+      }
+    }
+  }
+
+  if (!profile) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { profile },
+  }
+}
+
+export default function ProfilePage({ profile }: Props) {
   const { loading } = useAuth()
-  const router = useRouter()
-  const { username } = router.query
 
   if (!loading) return <div className="font-mono">Loading...</div>
 
   return (
-    <CustomLayout title="Devgram">
-      <div>{username}</div>
+    <CustomLayout title={profile.name}>
+      <div>{profile.username}</div>
     </CustomLayout>
   )
 }
