534218325f5057d4891baf0d883ced53898b9f3a
lbavendanos <lbavendanos@gmail.com>
2021-02-13 16:09:21 -0500

    Add firebase config

--- utils/firebase/client.ts
+++ utils/firebase/client.ts
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDP0qWFqSRseYNJE1dkznMU_Ri27x6iuJE',
-  authDomain: 'devgram-e991b.firebaseapp.com',
-  projectId: 'devgram-e991b',
-  storageBucket: 'devgram-e991b.appspot.com',
-  messagingSenderId: '1006827916180',
-  appId: '1:1006827916180:web:75ad21f7f7e39532e05d81',
-  measurementId: 'G-CLM3YVWVEP',
-}
+import firebaseConfig from '@/firebase.config'
 
 const app =
   firebase.apps.length > 0
