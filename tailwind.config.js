11cb10444c4205ed7d48d0bf12587db7b9859e1e
lbavendanos <lbavendanos@gmail.com>
2021-02-12 23:55:16 -0500

    Fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      screens: {
+        sm: '600px',
+        md: '700px',
+        lg: '800px',
+        xl: '900px',
+      },
+    },
     extend: {},
   },
   variants: {
